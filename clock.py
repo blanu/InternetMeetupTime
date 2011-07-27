7ddf2a935ea0ca6029ef3ce4ba2dadced2d23dcb
Brandon Wiley <brandon@blanu.net>
2011-07-27 15:24:47 -0500

    curses clock

---
+++ clock.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+if __name__=='__main__':
+  import curses
+
+  win=curses.initscr()
+  win.clear()
+
+  try:
+   while True:
+    sec=int(time.time())
+    esec=sec%86400
+    s=str(esec)
+    while len(s)<5:
+      s='0'+s
+    h=s[:1]
+    m=s[1:3]
+    s=s[3:]
+    s='~'+h+':'+m+':'+s
+
+    win.addnstr(11, 35, s, len(s))
+    win.move(0,0)
+    win.refresh()
+    time.sleep(1)
+  except:
+    curses.endwin()
+

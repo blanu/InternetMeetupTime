fe14b947178fa7fff58c197c46c82a154df16b4b
Brandon Wiley <brandon@blanu.net>
2011-07-28 16:00:07 -0500

    Standards compliance

--- clock.rb
+++ clock.rb
@@ -2,11 +2,11 @@ Shoes.app :width => 300, :height => 100 do
   def calc_time
     t = Time.new.utc.to_i%86400
     t = t.to_s
-    t = "@ "+t[0..2]+" : "+t[3..5]
+    t = "~"+t[0..2]+"."+t[3..5]
   end
   background "#BE8"  
   stack do
-    @title = title "New Internet Time!",
+    @title = title "Internet Meetup Time!",
       :align => "center",
       :size => 15
 

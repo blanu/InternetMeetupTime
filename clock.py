51f74f756c94321597aace2dc6027e71c3276ba9
Brandon Wiley <brandon@blanu.net>
2011-07-27 23:23:59 -0500

    Switched from H:MM:SS format to MMM.SS format.

--- clock.py
+++ clock.py
@@ -12,12 +12,13 @@ if __name__=='__main__':
     sec=int(time.time())
     esec=sec%86400
     s=str(esec)
-    while len(s)<5:
-      s='0'+s
+#    while len(s)<5:
+#      s='0'+s
     h=s[:1]
     m=s[1:3]
     s=s[3:]
-    s='~'+h+':'+m+':'+s
+#    s='~'+h+':'+m+':'+s
+    s='~'+h+m+'.'+s
 
     win.addnstr(11, 35, s, len(s))
     win.move(0,0)

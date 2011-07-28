7abbcad5f4935498f9a931d2d6e8eef4bc0d4d50
Brandon Wiley <brandon@blanu.net>
2011-07-28 15:45:29 -0500

    Added stopwatch

---
+++ stopwatch.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+if __name__=='__main__':
+  import curses
+
+  win=curses.initscr()
+  win.clear()
+
+  sec=0
+
+  try:
+   while True:
+    esec=sec%86400
+    s=str(esec)
+    while len(s)<5:
+      s='0'+s
+    h=s[:1]
+    m=s[1:3]
+    s=s[3:]
+#    s='~'+h+':'+m+':'+s
+    s='~'+h+m+'.'+s
+
+    win.addnstr(11, 35, s, len(s))
+    win.move(0,0)
+    win.refresh()
+    time.sleep(1)
+    sec=sec+1
+  except:
+    curses.endwin()
+
